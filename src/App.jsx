0d5aa92e2455954f0af639356bd93a956f16bbef
loganathanramsaran <loguflower3@gmail.com>
2025-06-20 00:14:38 +0530

    refactor: adjust header and footer positioning, update sidebar styles for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -109,7 +109,7 @@ function App() {
 
   return (
     <div className="min-h-screen bg-mint-light dark:bg-gray-900 transition-colors duration-300">
-      <header className="flex justify-between items-center px-4 py-3 shadow-md bg-emerald-400 dark:bg-gray-800">
+      <header className="flex fixed left-0 right-0 top-0 z-50 justify-between items-center px-4 py-3 shadow-md bg-emerald-400 dark:bg-gray-800">
         <div className="flex items-center gap-2 text-gray-800 dark:text-white">
           <LayoutDashboard className="w-6 h-6 text-emerald-600 dark:text-emerald-400" />
           <h1 className="text-2xl font-bold">Kanban Board</h1>
@@ -129,12 +129,12 @@ function App() {
         </div>
       </header>
 
-      <div className="flex">
+      <div className="flex pt-16">
         <Sidebar onSelect={handleSidebarSelect} open={sidebarOpen} setOpen={setSidebarOpen} />
         <main className="w-full mx-auto min-h-screen">{renderView()}</main>
       </div>
 
-      <footer className="bg-emerald-400 text-center dark:bg-gray-900 dark:text-gray-400 py-4 border-t border-gray-200 dark:border-gray-700">
+      <footer className="bg-emerald-400 text-center fixed bottom-0 left-0 right-0 dark:bg-gray-900 dark:text-gray-400 py-1 border-t border-gray-200 dark:border-gray-700 ">
         &copy; {new Date().getFullYear()} Kanban Board
       </footer>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -28,8 +28,8 @@ export default function Sidebar({ onSelect }) {
     <div className="sidebar">
       <button
         onClick={toggleSidebar}
-        className=" z-50 bg-emerald-600 hover:bg-emerald-700 text-white p-2 rounded-lg 
-        shadow-md transition lg:hidden ml-2 mt-4"
+        className=" z-20 bg-emerald-600 hover:bg-emerald-700 text-white p-2 rounded-lg 
+        shadow-md transition lg:hidden ml-2 mt-4 scroll:-0 fixed lg:static"
         aria-label="Toggle sidebar"
       >
         {open ? <X size={20} /> : <Menu size={20} />}
@@ -46,7 +46,7 @@ export default function Sidebar({ onSelect }) {
       {/* Sidebar */}
       <aside
         ref={sidebarRef}
-        className={`fixed top-0 left-0 min-h-full w-52 bg-emerald-100 dark:bg-gray-900 text-gray-900
+        className={`fixed top-0 left-0 min-h-screen w-52 bg-emerald-100 dark:bg-gray-900 text-gray-900
             dark:text-white shadow-lg transform transition-transform duration-300 ease-in-out 
             z-50 ${open ? "translate-x-0" : "-translate-x-full"} 
             lg:translate-x-0 lg:static lg:block`}
